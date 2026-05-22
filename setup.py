adacca83eb14f253e5f9b2a878367bcf8d32a050
TLibs <tlibs@hotmail.com>
2026-05-22 10:32:30 -0400

    Update install_requires in setup.py with dependencies

--- setup.py
+++ setup.py
@@ -15,8 +15,23 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_namespace_packages(where='src'),
     package_dir={"": "src"},
-    install_requires=install_requires,  # add any additional packages that
-    # needs to be installed along with your package. Eg: 'caer'
+    install_requires=[
+        "dnspython>=2.6.1",
+        "greenlet>=3.0.3",
+        "numpy>=1.26.4",
+        "pandas>=2.2.1,<3.0.0",
+        "psycopg2>=2.9.9,<3.0.0",
+        "pymongo>=4.6.2,<5.0.0",
+        "pymssql>=2.2.11,<3.0.0",
+        "pyodbc>=5.1.0,<6.0.0",
+        "python-dateutil>=2.9.0.post0",
+        "python-dotenv>=1.0.1",
+        "pytz>=2024.1",
+        "six>=1.16.0",
+        "SQLAlchemy>=2.0.29,<3.0.0",
+        "typing_extensions>=4.10.0",
+        "tzdata>=2024.1",
+    ],
     python_requires=">=3.10",
     keywords=["python", "sqlalchemy", "pandas", "mongo"],
     classifiers=[
